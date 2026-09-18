37a4c2a596d424aed5a4626ba8f975c4563ba93a
AbderrahmaneOd <abdoouaday361@gmail.com>
2026-09-18 12:55:24 +0100

    [add dockerfile for the frontend]

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,6 +11,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // This option allows you to build a standalone Next.js application that can be deployed without the need for a Node.js server. 
+  // It includes all necessary files and dependencies in the output directory, making it easier to deploy to various environments.
+  output: "standalone", 
 };
 
 export default nextConfig;
